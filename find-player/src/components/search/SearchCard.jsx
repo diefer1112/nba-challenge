cd480fa8d22db19397e46ca32c2bf3c2f3df6485
Diego Carvajal <diegofer.carvajal@gmail.com>
2022-03-18 11:39:09 -0500

    fix className on div to searchCard

--- find-player/src/components/search/SearchCard.jsx
+++ find-player/src/components/search/SearchCard.jsx
@@ -11,7 +11,7 @@ export const SearchCard = (
     return (
         <div className='col-12'>
             <div className=' bg-light mb-3'>
-                <div class="card-header text-right"> {firstName} {lastName}</div>
+                <div className="card-header text-right"> {firstName} {lastName}</div>
                 <div className='card-body'>
                     <p className='card-text m-2'> Height (inch): {hInch}</p>
                     <p className='card-text m-2'> Height (meter): {hMeters}</p>
